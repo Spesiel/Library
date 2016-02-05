5d901f4fe8ffe6133f94ba289f2976bb68408fd8
Pénélope Blossom <luckypenny.blossom+github@gmail.com>
2016-02-05 01:37:45 -0800

    Basics of the project

---
+++ LibraryReference/Constants.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+
+namespace Library.Resources
+{
+    /// <summary>
+    /// Contains all the fixed elements used through the application
+    /// </summary>
+    public static class Constants
+    {
+        /// <summary>
+        /// The base path for all the files
+        /// </summary>
+        //FIXME Change the base folder path
+        private static readonly string BasePath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + Path.DirectorySeparatorChar;
+
+        /// <summary>
+        /// Path to the folder containing the Cache
+        /// </summary>
+        public static readonly string CachePath = BasePath + Properties.CacheNamePrefix + Path.DirectorySeparatorChar;
+
+        /// <summary>
+        /// Path to the file containing the Settings
+        /// </summary>
+        public static readonly string SettingsFile = BasePath + Properties.SettingsFile;
+    }
+}
