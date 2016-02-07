c833087e27e4cd3d31c0b46f8fef9feb0aaf70a8
Pénélope Blossom <luckypenny.blossom+github@gmail.com>
2016-02-07 04:16:57 -0800

    Basics for the queue system

--- LibraryQueuing/Properties/AssemblyInfo.cs
+++ LibraryQueuing/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("LibraryQueue")]
+[assembly: AssemblyTitle("LibraryQueuing")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("LibraryQueue")]
+[assembly: AssemblyProduct("LibraryQueuing")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
