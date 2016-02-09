a6578b80694952bd724dad89682b59cdda6d794b
Pénélope Blossom <luckypenny.blossom+github@gmail.com>
2016-02-09 07:19:13 -0800

    Basic

---
+++ LibraryPreviewer/Main.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Library.Previewer
+{
+    public partial class Main : Form
+    {
+        #region Constructors
+
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        #endregion Constructors
+    }
+}
