ea009a937606814a972fc6e7fc370babd35544e2
Pénélope Blossom <luckypenny.blossom+github@gmail.com>
2016-02-09 06:49:41 -0800

    Basic

---
+++ LibraryGUI/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Library.UserInterface
+{
+    internal static class Program
+    {
+        #region Methods
+
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        private static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Main());
+        }
+
+        #endregion Methods
+    }
+}
