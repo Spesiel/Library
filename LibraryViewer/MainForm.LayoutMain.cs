46e7583dfb2482f551683becd1e5d0731b40c045
Pénélope Blossom <luckypenny.blossom+github@gmail.com>
2016-02-23 11:58:53 -0800

    Preparation for the different layouts

---
+++ LibraryViewer/MainForm.LayoutMain.cs
@@ -0,0 +1,31 @@
+﻿using Library.Controls;
+using System.Windows.Forms;
+
+namespace Library.Viewer
+{
+    public partial class MainForm
+    {
+        #region Fields + Properties
+
+        private LayoutMainForm layoutMain;
+
+        #endregion Fields + Properties
+
+        #region Methods
+
+        public void DisplayMain()
+        {
+            Controls.Remove(layoutWait);
+            layoutWait = null;
+
+            layoutMain = new LayoutMainForm();
+            layoutMain.Dock = DockStyle.Fill;
+            layoutMain.ResumeLayout(true);
+
+            Controls.Add(layoutMain);
+            ResumeLayout(true);
+        }
+
+        #endregion Methods
+    }
+}
---
+++ LibraryViewer/MainForm.LayoutWait.cs
@@ -0,0 +1,31 @@
+﻿using Library.Controls;
+using System.Windows.Forms;
+
+namespace Library.Viewer
+{
+    public partial class MainForm
+    {
+        #region Fields + Properties
+
+        private LayoutWait layoutWait;
+
+        #endregion Fields + Properties
+
+        #region Methods
+
+        public void DisplayWait(int total = 0)
+        {
+            Controls.Remove(layoutMain);
+            layoutMain = null;
+
+            layoutWait = new LayoutWait(total);
+            layoutWait.Dock = DockStyle.Fill;
+            layoutWait.ResumeLayout(true);
+
+            Controls.Add(layoutWait);
+            ResumeLayout(true);
+        }
+
+        #endregion Methods
+    }
+}
